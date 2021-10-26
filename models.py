819ccc2683951d581cd4b40b5854ae5a5df001ab
MurrayCode <Murraystewart94@hotmail.co.uk>
2021-10-26 12:10:52 +0100

    Finished User and Post models and added check password function

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import sqlalchemy as sql
 import sqlalchemy.orm as orm
 import passlib.hash as hash
+from sqlalchemy.sql.schema import Index
 import database as db
 import datetime as dt
 
@@ -11,6 +12,14 @@ class User(db.Base):
     email = sql.Column(sql.String, unique=True, Index=True)
     password = sql.Column(sql.String)
     date_created = sql.Column(sql.DateTime, default= dt.datetime.utcnow)
+    posts = orm.relationship("Post", back_populates="owner")
 
+    def check_password(self, password: str):
+        return hash.bcrypt.verify(password, self.hashed_password)
 class Post(db.Base):
-    pass
+    __tablename__ = "posts"
+    id = sql.Column(sql.Integer, primary_key=True, Index=True)
+    user_id = sql.Column(sql.Integer, sql.ForeignKey("users.id"))
+    post_body = sql.Column(sql.String, index=True)
+    date_created = sql.Column(sql.DateTime, default=dt.datetime.utcnow)
+    owner = orm.relationship("User", back_populates="posts")
