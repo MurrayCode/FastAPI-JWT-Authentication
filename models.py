fddb8ae5ce70c9e49ef1229763d4c4b882e707b7
MurrayCode <Murraystewart94@hotmail.co.uk>
2021-10-26 12:06:11 +0100

    crated user model

--- models.py
+++ models.py
@@ -2,9 +2,15 @@ import sqlalchemy as sql
 import sqlalchemy.orm as orm
 import passlib.hash as hash
 import database as db
+import datetime as dt
 
 class User(db.Base):
-    pass
+    __tablename__ = "users"
+    id = sql.Column(sql.Integer, primary_key=True, index=True)
+    name = sql.Column(sql.String, unique=False)
+    email = sql.Column(sql.String, unique=True, Index=True)
+    password = sql.Column(sql.String)
+    date_created = sql.Column(sql.DateTime, default= dt.datetime.utcnow)
 
 class Post(db.Base):
     pass
